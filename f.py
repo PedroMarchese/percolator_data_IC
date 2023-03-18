350e7604dacb9eb88352c2e51aba8bd5cf8c8ab1
Pedro Marchese <phmarchese@hotmail.com>
2023-03-17 23:29:38 -0300

    uepa

--- f.py
+++ f.py
@@ -4,7 +4,7 @@ import numpy as np
 import threading as t
 from Bio import SeqIO
 
-n_threads = 30
+n_threads = 10
 base_path = os.getcwd() or '/home/farminfo/PMarchese/percolator_data_IC'
 
 class Protein:
@@ -19,45 +19,14 @@ def percentage_finder(all_proteins: pd.Series):
     records = SeqIO.parse(f'{base_path}/data/mtb_proteome_cat.fasta', 'fasta')
     found_proteins = []
 
-    annotated_counter = 0
-    for i, record in enumerate(records):
-        identifiers = str(record.description)
-        sequences = str(record.seq)
-
-        if i % 100 == 0:
-            output_string = ''
-            for protein in found_proteins:
-                output_string += protein.get_string()
-
-            with open(f'{base_path}/output/outfile.fasta', 'a', encoding='utf-8') as outfile:
-                outfile.write(output_string)
-
-            found_proteins = []
-
-        # TRATA ESSES DADOS
-        if all_proteins.str.contains(identifiers).any():
-            print(f'[{annotated_counter}/5609] {identifiers} encontrado!')
-            
-            found_proteins.append(Protein(identifiers, sequences))
-            annotated_counter += 1
-
-    print(f'{round(annotated_counter / all_proteins.size * 100, 3)}% anotadas!')
 
 def main():
-    with open(f'{base_path}/data/entire_output.txt', 'r') as f:
-        lines = f.readlines()
         
-    chunks = np.array_split(np.array(lines), n_threads)
-    df = pd.DataFrame({'protein_ids': lines })
-    ps = df['protein_ids']
+    df = pd.read_csv(f'{base_path}/data/entire_output.txt', header=0)
+    anno = df[df['proteinId'].str.contains('ANNO')]
+    mask = anno.duplicated(keep=False)
+    unique_anno = anno[~mask]
 
-    threads = []
-    for i, chunk in enumerate(chunks):
-        threads.append(
-            t.Thread(target=protein_gather, args=(chunk))
-        )
-
-    percentage_finder(ps)
-
+    unique_anno.to_csv(f'{base_path}/output/filtered_anno.csv', index=False)
 
 main()
